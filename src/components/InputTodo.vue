5a7b9fff75e00246385d4bfafdd5a55685fb66fc
Abolfazl <a.baghshahi1327@gmail.com>
2021-04-28 02:22:24 +0430

    initial

---
+++ src/components/InputTodo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="input-todo">
+    <input
+      @keydown.enter="addInput()"
+      type="text"
+      v-model="input"
+      ref="input"
+    />
+    <button @click="addInput()">sumbit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputTodo",
+  data() {
+    return {
+      input: "",
+    };
+  },
+
+  methods: {
+    addInput() {
+      if (!this.input) return;
+
+      this.$emit("add-item", this.input);
+      this.input = "";
+    },
+  },
+};
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="todo-list">
+    todo lsit
+    <InputTodo @add-item="$emit('add-item', $event)" />
+    <TodoItems :list="list" @delete-item="$emit('delete-item', $event)" />
+  </div>
+</template>
+
+<script>
+import InputTodo from "./InputTodo";
+import TodoItems from "./TodoItems";
+
+export default {
+  name: "TodoList",
+  components: { InputTodo, TodoItems },
+  props: {
+    list: Array,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
