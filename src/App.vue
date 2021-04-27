f2ee0c9faa2c0badde601b3f93b00c0133a53fdb
Abolfazl <a.baghshahi1327@gmail.com>
2021-04-28 02:35:35 +0430

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,19 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <TodoList
-      :list="todoList"
-      @add-item="handleAdd"
-      @delete-item="handleDelete($event)"
-    />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import TodoList from "./components/TodoList";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    // HelloWorld
-    TodoList,
-  },
-
-  data() {
-    return {
-      todoList: [
-        {
-          title: "first one",
-          checked: false,
-        },
-        {
-          title: "second one",
-          checked: false,
-        },
-      ],
-    };
-  },
-  methods: {
-    handleAdd(payload) {
-      this.todoList.push({
-        title: payload,
-        checked: false,
-      });
-      // this.todoList = [...this.todoList, { title: valu, checked: false }];
-    },
-    handleDelete(title) {
-      this.todoList = this.todoList.filter((item) => item.title !== title);
-    },
-  },
-};
+    HelloWorld
+  }
+}
 </script>
 
 <style>
